8c2c198b6e7684a7777a33d27a11a3d30c817a8b
JANVI CHAVADA <19ce059@gardividyapith.ac.in>
2022-07-12 19:04:28 +0530

    second commit

---
+++ BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.Viewmodels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly BookStoreContext _context = new BookStoreContext();
+    }
+}
